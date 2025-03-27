c52304a950158ee38314e9b975d84a0a4a6b0621
Marlon Lacerda <marlonlac10@hotmail.com>
2025-03-27 20:22:41 -0300

    Primeiro commit

---
+++ src/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const path = require('path'); 
+const app = express()
+const port = 3000
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'login.html'));
+});
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, 'register.html'));
+});
+app.listen(port, () => {
+  console.log(`App de exemplo esta rodando na porta ${port}`)
+})
